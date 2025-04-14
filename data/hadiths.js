44a4deb2c794423cefc014600586b41576f602af
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-04-14 16:51:46 +0200

    add zekr

---
+++ data/hadiths.js
@@ -0,0 +1,10 @@
+[
+    {
+        "hadith": "The Prophet Muhammad (peace be upon him) said: \"Actions are judged by intentions, and each person will get the reward according to their intention.\"",
+        "source": "Sahih Bukhari 1",
+        "description": "This hadith emphasizes the importance of intentions behind actions. It teaches that the value of an action is determined by the intention behind it, not just the action itself."
+    },
+    {
+        
+    }
+]
