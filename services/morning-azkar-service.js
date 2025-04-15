3c0a0cb0ff95b52277350ab81a4b05c07b522860
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-04-15 22:12:19 +0200

    add all

--- services/morning-azkar-service.js
+++ services/morning-azkar-service.js
@@ -1,4 +1,4 @@
-const { validationResult } = require("express-validator");
+/*const { validationResult } = require("express-validator");
 const Azkar = require("../models/morning-azkar");
 const asyncHandler = require("express-async-handler");
 const Apierror = require("../Utils/api-error");
@@ -32,6 +32,34 @@ const getallmorningazkarRoute = asyncHandler(async (req, res, next) => {
         zekr,
         currentPage: page,
         totalAzkar: morningazkar.length
+    });
+});
+
+module.exports = {
+    getallmorningazkarRoute
+};*/
+const { validationResult } = require("express-validator");
+const Azkar = require("../models/morning-azkar");
+const asyncHandler = require("express-async-handler");
+const Apierror = require("../Utils/api-error");
+
+const getallmorningazkarRoute = asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return next(new Apierror(errors.array()[0].msg, 400));
+    }
+
+    // جلب كل الأذكار من قاعدة البيانات دفعة واحدة
+    const morningazkar = await Azkar.find();
+
+    if (!morningazkar || morningazkar.length === 0) {
+        return next(new Apierror("لا توجد أذكار متاحة!", 404));
+    }
+
+    // إرجاع جميع الأذكار مع العدد الكلي للأذكار
+    res.status(200).json({
+        azkar: morningazkar,  // إرسال جميع الأذكار
+        totalAzkar: morningazkar.length // إرسال العدد الكلي للأذكار
     });
 });
 
