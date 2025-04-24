942cc4f4d857eb7a3df6badffeaed1b6bdb6f55f
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-04-24 15:23:24 +0200

    cc

--- seeder.js
+++ seeder.js
@@ -6,12 +6,10 @@ const connectToDb = require('./config/database');
 const MorningAzkarModel = require('./models/morning-azkar');
 const EveningAzkarModel = require('./models/evening-azkar');
 const BeforeSleepAzkarModel = require('./models/before-sleep');
-const PrayerModel = require('./models/prayers-model');
 
 const morningAzkarData = require('./data/morning-azkar');
 const eveningAzkarData = require('./data/evening-azkar');
 const beforeSleepAzkarData = require('./data/before-sleep-azkar');
-const prayerData = require('./data/prayers');
 
 connectToDb();
 
@@ -51,17 +49,6 @@ const importBeforeSleepAzkar = async () => {
     }
 };
 
-const importprayers = async () => {
-    try {
-        await PrayerModel.deleteMany({});
-        await PrayerModel.insertMany(prayerData);
-        console.log('✅ Before Sleep Azkar imported successfully');
-        process.exit();
-    } catch (error) {
-        console.error('❌ Error importing before sleep azkar:', error);
-        process.exit(1);
-    }
-};
 
 const destroyAzkar = async () => {
     try {
@@ -84,9 +71,7 @@ if (arg === '-import-morning') {
     importEveningAzkar();
 } else if (arg === '-import-before-sleep') {
     importBeforeSleepAzkar();
-}  else if (arg === '-import-prayers') {
-    importprayers();
-}
+} 
 
 else if (arg === '-destroy') {
     destroyAzkar();
